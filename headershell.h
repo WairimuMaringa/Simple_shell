1220b6bcfc0f67ff443d52765ffb72cd29795186
WairimuMaringa <makena96wairimu@gmail.com>
2022-08-03 13:23:55 -0700

    added more functions to header

--- headershell.h
+++ headershell.h
@@ -16,3 +16,29 @@
 #include <limits.h>
 
 extern char **environ;
+
+/**
+ * struct bltn - variables with info on builtins
+ * @name: name
+ * @func: pointer
+ */
+
+typedef struct bltn
+{
+	char *name;
+	int (*func)(void);
+} builtin;
+
+int decoder(char **args);
+void free_space(int x, ...);
+void error_message(char **argv, char **args, int x);
+void error_number(int x);
+int exec(char **args);
+int find(char **args);
+int find_builtins(char **args);
+int find_dirs(char **instruction, char **args);
+void remove_equal_sign(char **string);
+int countchars(char *string);
+char **tokenize(char *string, int num);
+char *search_path(char *string);
+char *
