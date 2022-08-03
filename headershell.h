c5b402035bfcccd44a147ed6c320f55e109c3002
WairimuMaringa <makena96wairimu@gmail.com>
2022-08-03 10:50:52 -0700

    Start compiling header file

---
+++ headershell.h
@@ -0,0 +1,18 @@
+#ifndef HEADER_FILE_H
+#define HEADER_FILE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <errno.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <limits.h>
+
+extern char **environ;
